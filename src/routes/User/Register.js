56e0e79da3ae87ced1d531febe94a42285fe0eda
elvin <970099734@qq.com>
2018-07-13 11:08:39 +0800

    用户名只能是2~20位字符的字母，数字，下划线，减号

--- src/routes/User/Register.js
+++ src/routes/User/Register.js
@@ -244,8 +244,8 @@ export default class Register extends Component {
                 //   message: '请输入最多20位字符！',
                 // },
                 {
-                  pattern: /^[a-zA-Z0-9_-]{2,20}$/,
-                  message: '用户名只能包含 2~20位的字母，数字，下划线，减号',
+                  pattern: /^[\u4E00-\u9FA5_a-zA-Z0-9/-]{2,20}$/,
+                  message: '用户名只能是2~20位字符的字母，数字，下划线，减号',
                 },
               ],
             })(<Input size="large" placeholder="用户名 2-20位" />)}
